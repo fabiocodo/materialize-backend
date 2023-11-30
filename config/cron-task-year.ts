1ea26167d1e5b28a11e7ce6b966173ef54109420
fabiocodo <fabio.codo@gmail.com>
2023-11-30 18:41:12 -0300

    first commit

---
+++ config/cron-task-year.ts
@@ -0,0 +1,15 @@
+export default {
+
+    myJob: {
+      task: async ({ strapi }) => {
+       
+
+      
+       
+      },
+      options: {
+        rule: "10 7 * 1 * ",
+        //rule: "0 3 * 1 * ",
+      },
+    },
+  };
